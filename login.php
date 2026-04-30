3f118e6e47b92a20c57eebf2eeeab2e0162bb1a9
Christian0887 <ccariello@scuola-borsa.it>
2026-04-30 10:41:51 +0200

    fixconnessionenellepagine

--- login.php
+++ login.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
 // Connessione al database
-$conn = new mysqli("localhost", "root", "", "miosito");
-if($conn->connect_error) die("Errore DB: " . $conn->connect_error);
+require_once('config.php');
 
 $errore = "";
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
